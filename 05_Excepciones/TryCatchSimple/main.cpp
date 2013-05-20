8d795e13600be1fb71069b6f42d64ed006d7de77
Jaime Delgado Meraz <j2deme@gmail.com>
2013-05-20 13:46:06 -0500

    Minor changes

--- 05_Excepciones/TryCatchSimple/main.cpp
+++ 05_Excepciones/TryCatchSimple/main.cpp
@@ -9,34 +9,42 @@
 using namespace std;
 
 int main(int argc, char **argv) {
+	int entero;
+	double decimal;
 	try {
-		int entero;
-		double decimal;
 		cout << "Escribe un entero:";
 		cin >> entero;
 		cout << "Escribe un decimal:";
 		cin >> decimal;
 
 		if(entero > 20){
-			throw "Valor entero mayor a 20";
+			//throw "Valor entero mayor a 20";
+			throw entero;
 		}
 
 		if(entero < 10){
-			throw "Valor entero menor a 10";
+			throw entero;
+			//throw "Valor entero menor a 10";
 		}
 
 		if(decimal < 3.1416){
 			throw decimal;
 		}
+
+		throw false;
 	} catch(const char* e){
 		cout << e << endl;
 	} catch (int e) {
-		cout << "El valor " << e << " es mayor a 20." << endl;
+		cout << "Error de entero: " << e << endl;
+		entero = 10;
 	} catch (double e){
-		cout << "El valor " << e << " es menor que PI." << endl;
+		cout << "Error de double: " << e << endl;
+		decimal = 3.1416;
 	} catch (...){
 		cout << "Ocurrio un error" << endl;
 	}
+
+	cout << "Entero" << entero << "Decimal" << decimal << endl;
 	return 0;
 }
 
